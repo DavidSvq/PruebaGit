f8b6417e4ea60e102a09b361bded34e7095c4b67
David <davirt@hotmail.com>
2025-04-04 22:57:13 +0200

    este es el primer commit

---
+++ src/pruebaGit/PruebaGit.java
@@ -0,0 +1,19 @@
+package pruebaGit;
+
+public class PruebaGit{
+	String prueba;
+
+	public PruebaGit(String prueba) {
+		super();
+		this.prueba = prueba;
+	}
+
+	public String getPrueba() {
+		return prueba;
+	}
+
+	public void setPrueba(String prueba) {
+		this.prueba = prueba;
+	}
+	
+}
