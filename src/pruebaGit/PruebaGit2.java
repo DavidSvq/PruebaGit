c97a771ecbc5834065467ad8503b383efec6665a
David <davirt@hotmail.com>
2025-04-05 11:11:07 +0200

    Creación constructor pruebaGit2

--- src/pruebaGit/PruebaGit2.java
+++ src/pruebaGit/PruebaGit2.java
@@ -3,4 +3,11 @@ package pruebaGit;
 public class PruebaGit2 {
 	private String prueba;
 	private int nPrueba;
+	
+	public PruebaGit2(String prueba) {
+		super();
+		this.prueba = prueba;
+	}
+	
+	
 }
