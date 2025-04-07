e5d0b20e60eee343df55a44c469dc0eb11d4c307
David <davirt@hotmail.com>
2025-04-07 12:17:07 +0200

    Modificación PruebaGit3 desde el Main

--- src/pruebaGit/PruebaGit3.java
+++ src/pruebaGit/PruebaGit3.java
@@ -2,4 +2,13 @@ package pruebaGit;
 
 public class PruebaGit3 {
 	private String prueba;
+	private int nPrueba3;
+	
+	public PruebaGit3(String prueba, int nPrueba3) {
+		super();
+		this.prueba = prueba;
+		this.nPrueba3 = nPrueba3;
+	}
+	
+	
 }
